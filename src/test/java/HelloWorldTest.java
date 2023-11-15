d9fd0ed8eeedd4e5c1bd2aac55913e6df3a1a5f9
КЕА <KudlayEA@bcs.ru>
2023-11-15 23:32:34 +0700

    HelloWorldTest

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+//import org.junit.jupiter.api.Test;
+public class HelloWorldTest {
+    @Test
+    public  void HelloWorldTest(){
+        System.out.println("Hello from \u004B\u0075\u0064\u006C\u0061\u0069\u0020\u0045\u0076\u0067\u0065\u006E\u0069\u0069");
+    }
+}
