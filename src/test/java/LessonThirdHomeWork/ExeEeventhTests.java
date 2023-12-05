e2d274f47b04c3a0fd04aafb9fc4d0cfe262c0db
КЕА <KudlayEA@bcs.ru>
2023-12-06 02:33:06 +0700

    homework 11

---
+++ src/test/java/LessonThirdHomeWork/ExeEeventhTests.java
@@ -0,0 +1,33 @@
+package LessonThirdHomeWork;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Ex11: Тест запроса на метод cookie
+ * Необходимо написать тест, который делает запрос на метод: https://playground.learnqa.ru/api/homework_cookie
+ * Этот метод возвращает какую-то cookie с каким-то значением. Необходимо понять что за cookie и с каким значением, и зафиксировать это поведение с помощью assert.
+ * Результатом должна быть ссылка на коммит с тестом.
+ */
+public class ExeEeventhTests {
+    @Test
+    public void testHomeworkCookie() {
+        Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/homework_cookie")
+            .andReturn();
+        Map<String, String> cookies = response.getCookies();
+        String expectedCookieName = "HomeWork";
+        String expectedCookieValue = "hw_value";
+
+        assertEquals(200, response.statusCode(),
+            "Unexpected status code");
+        assertTrue(cookies.containsKey("HomeWork"), "cookies doesn't contains key 'HomeWork'");
+        assertTrue(cookies.containsValue("hw_value"), "cookies doesn't contains value 'hw_value'");
+    }
+}
