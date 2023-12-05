36ab209cb6ad41d692c9777de277010224f27214
КЕА <KudlayEA@bcs.ru>
2023-12-06 03:00:39 +0700

    homework 8

--- src/test/java/LessonSecondHomeWork/ExEighthTests.java
+++ src/test/java/LessonSecondHomeWork/ExEighthTests.java
@@ -9,46 +9,45 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Ex8: Токены
  * Иногда API-метод выполняет такую долгую задачу,
  * что за один HTTP-запрос от него нельзя сразу получить готовый ответ.
  * Это может быть подсчет каких-то сложных вычислений или необходимость собрать информацию по разным источникам.
- *
+ * <p>
  * В этом случае на первый запрос API начинает выполнения задачи, а на последующие ЛИБО говорит, что задача еще не готова,
  * ЛИБО выдает результат. Сегодня я предлагаю протестировать такой метод.
- *
+ * <p>
  * Сам API-метод находится по следующему URL: https://playground.learnqa.ru/ajax/api/longtime_job
- *
+ * <p>
  * Если мы вызываем его БЕЗ GET-параметра token, метод заводит новую задачу, а в ответ выдает нам JSON со следующими полями:
  * * seconds - количество секунд, через сколько задача будет выполнена
  * * token - тот самый токен, по которому можно получить результат выполнения нашей задачи
- *
- * 
+ * <p>
+ * <p>
  * В этом случае на первый запрос API начинает выполнения задачи, а на последующие ЛИБО говорит, что задача еще не готова,
  * ЛИБО выдает результат. Сегодня я предлагаю протестировать такой метод.
- * 
+ * <p>
  * Сам API-метод находится по следующему URL: https://playground.learnqa.ru/ajax/api/longtime_job
- * 
+ * <p>
  * Если мы вызываем его БЕЗ GET-параметра token, метод заводит новую задачу, а в ответ выдает нам JSON со следующими полями:
  * * seconds - количество секунд, через сколько задача будет выполнена
  * * token - тот самый токен, по которому можно получить результат выполнения нашей задачи
- * 
+ * <p>
  * Если же вызвать API-метод, УКАЗАВ GET-параметром token, то мы получим следующий JSON:
  * * error - будет только в случае, если передать token, для которого не создавалась задача.
  * В этом случае в ответе будет следующая надпись - No job linked to this token
  * * status - если задача еще не готова, будет надпись Job is NOT ready, если же готова - будет надпись Job is ready
  * * result - будет только в случае, если задача готова, это поле будет содержать результат
- *
- * 
+ * <p>
+ * <p>
  * Наша задача - написать тест, который сделал бы следующее:
  * 1) создавал задачу
  * 2) делал один запрос с token ДО того, как задача готова, убеждался в правильности поля status
  * 3) ждал нужное количество секунд с помощью функции Thread.sleep() - для этого надо сделать import time
  * 4) делал бы один запрос c token ПОСЛЕ того, как задача готова, убеждался в правильности поля status и наличии поля result
- *
+ * <p>
  * Как всегда, код нашей программы выкладываем ссылкой на коммит.
  */
 public class ExEighthTests {
@@ -59,7 +58,7 @@ public class ExEighthTests {
         JsonPath response = RestAssured
             .get("https://playground.learnqa.ru/ajax/api/longtime_job")
             .jsonPath();
-
+        response.prettyPrint();
         String token = response.get("token");
         int time = response.get("seconds");
         //Проверка
@@ -69,20 +68,21 @@ public class ExEighthTests {
 
         Map<String, String> parameters = new HashMap<>();
         parameters.put("token", token);
-        // делает один запрос с token ДО того, как задача готова, убеждается в правильности поля status
-        JsonPath response1 = RestAssured
+
+        // запрос с token ДО того, как задача готова, убеждается в правильности поля status
+        JsonPath responseOne = RestAssured
             .given()
             .queryParams(parameters)
             .get("https://playground.learnqa.ru/ajax/api/longtime_job")
             .jsonPath();
 
-        String text = response1.get("status");
-        assertEquals("Задача не готова", text);
+        String text = responseOne.get("status");
+        assertEquals("Job is NOT ready", text);
 
-        // ждет нужное количество секунд с помощью функции time.sleep() - для этого надо сделать import time
+        // ждет нужное количество секунд с помощью функции time.sleep()
         Thread.sleep(time * 1000);
 
-        // делает один запрос c token ПОСЛЕ того, как задача готова, убеждается в правильности поля status и наличии поля result
+        //убеждается в правильности поля status и наличии поля result
         JsonPath getResponseToken = RestAssured
             .given()
             .queryParams(parameters)
@@ -93,7 +93,7 @@ public class ExEighthTests {
         String resultStatus = getResponseToken.get("status");
 
         Validate.notNull(resultant);
-        assertEquals("Задача готова", resultStatus);
+        assertEquals("Job is ready", resultStatus);
 
         System.out.println("Result:" + resultant);
         System.out.println(resultStatus);
